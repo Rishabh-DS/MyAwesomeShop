7d515d79a9e52ca0b09060da6052002ba17c25cb
Rishabh <rishabhgarg313@gmail.com>
2021-06-24 19:10:15 +0530

    added domain name in setting.py file

--- ecomweb1/settings.py
+++ ecomweb1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 't_qj&8@tgxouw)80832nqg+lqtsl7a-%7-@5!g75it#%y%#!9i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1' , 'meridukaan.herokuapp.com']
 
 
 # Application definition
